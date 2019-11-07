48a664c2d4775fd27b995f45ed7156db15cbe635
P-CAmilE <43110057+P-CAmilE@users.noreply.github.com>
2019-11-07 17:20:33 +0800

    update of Parser 19/11/7

--- StandardML/Parser.h
+++ StandardML/Parser.h
@@ -1,7 +1,5 @@
 /*---------------------------------------------------
-	the head file of Parser
-
-	
+	the head file of Parser	
 ---------------------------------------------------*/
 
 #pragma once
@@ -16,10 +14,74 @@ public:
 	
 
 private:
-	int curToken = 0;
-
+	int CurTok = 0;
+	map<char, int> BinopPrecedence;
 	int getNextToken();
+	int GetTokPrecedence();
+
+	/*-------------------------------------------
+					expression
+	-------------------------------------------*/
+	/*----------------constant----------------*/
+	// parse bool value
+	unique_ptr<ExprAST> ParseBoolenExpr();
+	// parse int and real value
+	unique_ptr<ExprAST> ParseNumberExpr();
+	// parse char and string value
+	unique_ptr<ExprAST> ParseStringExpr();
+	// top level parser of constant
+	unique_ptr<ExprAST> ParseConstExpr();
+
+	/*------------Variable and function call------------*/
+	/// VariableExpr
+	///   ::= Variable
+	/// NOTE: function call lays below
+	///   ::= Variable Expression
+	///   ::= Variable '(' ')'
+	///   ::= Variable '(' Expression (',' Expression)* ')' 
+	unique_ptr<ExprAST> ParseVariableExpr();
+
+	/*---------------If and Let Expression--------------*/
+	/// IfExpr ::= tok_if Expression tok_then Expression tok_else Expression
+	unique_ptr<ExprAST> ParseIfExpr();
+	/// LetExpr ::= tok_let Declartion tok_in Expression (';' Expression)* tok_end
+	unique_ptr<ExprAST> ParseLetExpr();
+
+	/*------------Paren and Primary Expression-------------*/
+	/// ParenExpr ::= '(' Expression ')'
+	unique_ptr<ExprAST> ParseParenExpr();
+	/// PrimaryExpr
+	///   ::= VariableExpr
+	///   ::= ConstExpr
+	///   ::= LetExpr
+	///   ::= ParenExpr
+	unique_ptr<ExprAST> ParsePrimaryExpr();
+	/*----------------Binary Expression----------------*/
+	/// BinaryExpr
+	///   ::= (Op PrimaryExpr)*	
+	unique_ptr<ExprAST> ParseBinaryExpr();
+
+	/*--------------Expression(top level)--------------*/
+	/// Expression 
+	///   ::= IfExpr
+	///   ::= PrimaryExpr BinaryExpr
+	unique_ptr<ExprAST> ParseExpression();
 
 
+	/*-------------------------------------------
+					declaration
+	-------------------------------------------*/
+	/*----------------Function----------------*/
+	/// Prototype
+	///   ::= id '(' ')'
+	///   ::= id '(' id (',' id)* ')'
+	///   ::= id id
+	unique_ptr<PrototypeAST> ParsePrototype();
+	/// Function ::= tok_fun Prototype '=' Expression
+	unique_ptr<DecAST> ParseFunction();
+
+	/*-----------------value-----------------*/
+	/// Value ::= tok_val id '=' Expression
+	unique_ptr<DecAST> ParseValue();
 };
 
